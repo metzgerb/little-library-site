e9f2609ed8b100065c17a80c2a0b02c9420db0ab
metzgerb <40550164+metzgerb@users.noreply.github.com>
2018-10-16 18:18:07 -0500

    Updated DB Con info and package
    Updated to point to correct database and to describe project in package file.

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
    connectionLimit   : 10,
    host              : 'classmysql.engr.oregonstate.edu',
-   user              : 'cs290_metzgerb',
+   user              : 'cs340_metzgerb',
    password          : '8545',
-   database          : 'cs290_metzgerb'
+   database          : 'cs340_metzgerb'
 });
 
 module.exports.pool = pool;
