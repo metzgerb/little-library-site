5e54989c33eb17c40cdabdb643461fcbd157abff
Brian Metzger <40550164+metzgerb@users.noreply.github.com>
2018-12-16 20:22:18 -0600

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
    connectionLimit   : 10,
-   host              : 'classmysql.engr.oregonstate.edu',
-   user              : 'cs340_metzgerb',
-   password          : '8545',
-   database          : 'cs340_metzgerb'
+   host              : 'YOUR_HOST',
+   user              : 'YOUR_USER',
+   password          : 'YOUR_PASSWORD',
+   database          : 'YOUR_DATABASE'
 });
 
 module.exports.pool = pool;
